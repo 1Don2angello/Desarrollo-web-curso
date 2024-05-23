00e8a2cf82632c4fdaf492ca514cca65ae62ce75
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-05-23 07:12:51 -0500

    footer

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Footer from './Footer/footer'
 import './App.css'
 
 function App() {
@@ -28,7 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
+
     </>
+
   )
 }
 
