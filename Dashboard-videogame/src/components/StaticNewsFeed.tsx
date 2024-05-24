270dd3d00a1041f7537f12238935691641c891e8
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-05-24 16:28:06 -0500

    upddate

--- Dashboard-videogame/src/components/StaticNewsFeed.tsx
+++ Dashboard-videogame/src/components/StaticNewsFeed.tsx
@@ -1,34 +1,14 @@
-// StaticNewsFeed.tsx
 import React from 'react';
 
 const StaticNewsFeed: React.FC = () => {
   return (
-    <div className="news-section">
-      <div className="game-card">
-         <h2>Noticias</h2>
+    <div className="news-feed-section">
+      <h3>Noticias</h3>
       <ul>
         <li>Constructive and destructive waves</li>
         <li>Another news item here</li>
         <li>More news updates</li>
       </ul>
-      </div>
-      <div className="game-card">
-         <h2>Noticias</h2>
-      <ul>
-        <li>Constructive and destructive waves</li>
-        <li>Another news item here</li>
-        <li>More news updates</li>
-      </ul>
-      </div>
-      <div className="game-card">
-         <h2>Noticias</h2>
-      <ul>
-        <li>Constructive and destructive waves</li>
-        <li>Another news item here</li>
-        <li>More news updates</li>
-      </ul>
-      </div>
-     
     </div>
   );
 };
