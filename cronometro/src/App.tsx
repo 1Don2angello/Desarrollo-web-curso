e90a5182d51076ac66ddf26faef96451f5227ff9
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-06-26 19:49:49 -0500

    create cronometro

--- cronometro/src/App.tsx
+++ cronometro/src/App.tsx
@@ -1,6 +1,5 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { TaskApp } from './components/estado'
 
@@ -8,15 +7,15 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      
       <TaskApp/>
       
     </>
